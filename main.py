9fabe6df038824a96a95d90f4605bde6ef4cd9e2
madhukaran <madhukaran.ramesh@gmail.com>
2025-01-05 22:50:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask,send_from_directory,jsonify
+app = Flask(__name__,
+            static_folder = "./frontend/build",
+            static_url_path="/",
+            )
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({"message": "404", "status": "error"}),404
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
